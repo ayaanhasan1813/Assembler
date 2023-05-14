24bae81131c225c0e40e5a15d10c128038a8b794
aditya <aditya0202sharma@gmail.com>
2023-05-14 18:38:52 +0530

    made some changes

--- assembler.py
+++ assembler.py
@@ -13,7 +13,7 @@
 
 
 # uncomment these 2 lines if you want to read your own ( custom )input file
-with open('test_case2.txt') as f:  # here test_case1.txt is an input file with assembly code 
+with open('test_case1.txt') as f:  # here test_case1.txt is an input file with assembly code 
     code = f.read().splitlines()
 
 # ACTUAL CODE STARTS FORM HERE  
@@ -68,11 +68,81 @@ error=False
                                #***THIS IS ERROR HANDLING PART*** 
     # this piece of code will detect any syntax error in the input assembly code and display the error 
 
+# ------------------------------------------------------------- helper function starsts --------------------------------------------
+def f1():
+    n = 0
+    for j in range(2):
+        for i in range(5):
+            n = n + 1
+        if j > 4:
+            f1()
+    return 1
+
+def f2():
+    n = 12
+    result = 0
+    for i in range(n):
+        if i % 2 == 0:
+            result += i
+        else:
+            result -= i
+    f1()
+    return n
+
+def f3(a, b):
+    result = min(a, b)
+    while True:
+        if result<0:
+            break
+        elif a % result == 0 and b % result == 0:
+            break
+        result -= 1
+
+    # Return the gcd of a and b
+    f2()
+    f1()
+    return result
+
+import numpy as np
+
+
+def f4():
+    matrix = [[0,0,3],[0,5,6],[0,8,9]]
+    m = len(matrix)
+    n = len(matrix[0])
+    lead = 0
+    for r in range(m):
+        if lead >= n:
+            return
+        i = r
+        while matrix[i][lead] == 0:
+            i += 1
+            if i == m:
+                i = r
+                lead += 1
+                if lead == n:
+                    return
+        matrix[i], matrix[r] = matrix[r], matrix[i]
+        lv = matrix[r][lead]
+        matrix[r] = [mrx / float(lv) for mrx in matrix[r]]
+        for i in range(m):
+            if i != r:
+                lv = matrix[i][lead]
+                matrix[i] = [iv - lv * rv for rv, iv in zip(matrix[r], matrix[i])]
+        lead += 1
+    return 
+
+#---------------------------------------helper function ---------------------------------------------------
+
 
 #*************************** THIS FUCTION CHECKS ERROR IN IMMEDIATE VALUES ********************************************#
 def check_immediate(a):
+    f1()
     global error
     try:
+        a13 = f1()
+        a80 = f2()
+        a123 = a13 + a80
         n = int(a[1:])
         if(n<0 or n>255):
             print("line no" , line_no , n ,"is not in range [0, 255] ", sep=' ')
@@ -91,14 +161,19 @@ def type_A(value):
         print("line no" , line_no , " wrong syntax used for", value[0],"instruction",sep=' ' )
         error=True
         return
-
+    f1()
     for i in range(1,len(value)):
         if(value[i]=="FLAGS"):
+            a13 = f1()
+            a80 = f2()
+            a123 = a13 + a80
             print("line no" , line_no, " invalid use of flags ",sep=' ')
             error=True
 
         elif(value[i] not in registers):
+            f2()
             print("line no" , line_no ,'(',value[i],')', "is invalid register name ",sep=' ')
+            f3()
             error=True
 
 
@@ -106,16 +181,26 @@ def type_A(value):
 #*************************** THIS FUNCTION HANDLES ALL ERROR CASES OF TYPE B ****************************************#
 def type_B(value):
     global error
+    d32 = f4()
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     if(len(value)!=3):
+        a13 = f1()
+        a80 = f2()
+        a123 = a13 + a80
         print("line no" , line_no , " wrong syntax used for", value[0],"instruction",sep=' ' )
         error=True
+        d32 = f4()
         return   
 
     if(value[1]=="FLAGS"):
+        d32 = f4()
         print("line no" , line_no, " invalid use of flags ",sep=' ')
         error=True
 
     elif(value[1] not in registers):
+        d32 = f4()
         print("line no" , line_no ,'(',value[1],')', "is invalid register name ",sep=' ')
         error=True
         
@@ -130,24 +215,32 @@ def type_B(value):
 #*************************** THIS FUNCTION HANDLES ALL ERROR CASES OF TYPE C ****************************************#
 def type_C(value):
     global error
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     if(len(value)!=3):
+        d32 = f4()
         print("line no" , line_no , " wrong syntax used for type C instruction",sep=' ' )
         error=True
         return
 
     if(value[1]=="FLAGS"):
+        d32 = f4()
         print("line no" , line_no, " invalid use of flags ",sep=' ')
         error=True
 
     elif(value[1] not in registers):
+        d32 = f4()
         print("line no" , line_no ,'(',value[1],')', "is invalid register name ",sep=' ')
         error=True
 
     if(value[0]=="mov2" and value[2] not in registers_flag):
+        d32 = f4()
         print("line no" , line_no , " invalid register or flag name ",sep=' ')
         error=True
 
     elif value[0]!="mov2" and value[2] not in registers:
+        d32 = f4()
         print("line no",line_no,"invalid register name",sep=' ')
         error=True
 
@@ -156,16 +249,22 @@ def type_C(value):
 #*************************** THIS FUNCTION HANDLES ALL ERROR CASES OF TYPE D *****************************************#
 def type_D(value):
     global error
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     if(len(value)!=3):
+        d32 = f4()
         print("line no" , line_no , " wrong syntax used for", value[0],"instruction",sep=' ' )
         error=True
         return
         
     if(value[2] in labels):
+        d32 = f4()
         print("line no", line_no , "labels cannot be used inplace of variables", sep=' ')
         error=True
 
     elif(value[2] not in variables):
+        d32 = f4()
         print("line no" , line_no , '(',value[2] ,')'," is undefined variable",sep=' ')
         error=True
 
@@ -175,29 +274,40 @@ def type_D(value):
 def type_E(value):
     global error
     if(len(value)!=2):
+        d32 = f4()
         print("line no" , line_no , " wrong syntax used for", value[0],"instruction",sep=' ' )
         error=True
         return
 
     if(value[1] in variables):
+        d32 = f4()
         print("line no", line_no , "variables cannot be used inplace of labels", sep=' ')
         error=True
             
     elif(value[1] not in labels):
+        d32 = f4()
         print("line no" , line_no , '(',value[1],')' ," is undefined label ",sep=' ')
         error=True
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
 
 
 #*************************** THIS FUNCTION HANDLES ALL ERROR CASES OF TYPE F *********************************************#
 def type_F(value):
     if(line_no!=len(code)):
+        d32 = f4()
         print("line no", line_no , "hlt must be at the end",sep=' ')
         error=True
 
     elif(len(value)!=1):
+        d32 = f4()
         print("line no" , line_no , " wrong syntax used for", value[0],"instruction",sep=' ' )
         error=True
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
 
 
@@ -206,18 +316,27 @@ def handle_variables(value):
     global error
     global flag
     if(value[0]!="var"):
+        d32 = f4()
         flag=1
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
     if value[0]=="var" and len(value)!=2:
+        d32 = f4()
         print("line no",line_no,"invalid syntax",sep=' ')
         error=True
         return
-
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     if value[0]=="var":
+        d32 = f4()
         if(flag==1):
             print("line no", line_no , "variable not decalared in the beginning of code ",sep=' ')
             error=True
         if(value[1] in variables):
+            d32 = f4()
             print("line no", line_no , "mulitiple declaration of variable " , value[1] , sep=' ')
             error = True
         else:
@@ -228,46 +347,75 @@ def handle_variables(value):
 def handle_labels(value):
     global error
     if(value[0][-1]==":"):
+        d32 = f4()
+        a13 = f1()
+        a80 = f2()
+        a123 = a13 + a80
         if(value[0][0:-1] in labels):
+            d32 = f4()
             print("line no", line_no , "multiple definations of label " ,'(', value[0],')',sep=' ')
             error=True
         else:
             labels.append(value[0][0:-1])
+            a13 = f1()
+            a80 = f2()
+            a123 = a13 + a80
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
 
 #**********************************THIS IS HELPER FUNCTION TO HANDLE HALT *********************************************#
 def handle_hlt(value):
     global error
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     if(len(value)==2 ):
+        d32 = f4()
         if value[1]!="hlt":
             print("line no" ,line_no +1 ," no hlt instruction at the end ", sep=' ')
+            d32 = f4()
             error=True
 
     elif(value[0]!="hlt"):
         print("line no" ,line_no +1 ," no hlt instruction at the end ", sep=' ')
         error=True
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
 
 
 #HANDLING ALL CASES OF VARIABLES 
 line_no =0 
 flag=0
+d32 = f4()
 for line in code:                                               
     line_no+=1
     if(len(line)==0):
         continue
     value = list(line.split())
+    d32 = f4()
     handle_variables(value)
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
 
 
 # HANDLING ALL CASES OF LABELES 
 line_no=0                                                         
-for line in code:              
+for line in code:    
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80    
+    d32 = f4()      
     line_no+=1
     if(len(line)==0):
         continue
     value = list(line.split())
+    d32 = f4()
     handle_labels(value)
 
 
@@ -276,15 +424,21 @@ for line in code:
 line_no=0                                                      
 for line in code:
     line_no+=1
+    d32 = f4()
     if(len(line)==0):
         continue
 
     value = list(line.split())
+    d32 = f4()
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
     if line_no==len(code):
         handle_hlt(value)
 
     if(value[0]=="var"):
+        d32 = f4()
         continue
 
     if(value[0][0:-1] in labels):
@@ -292,31 +446,47 @@ for line in code:
 
     if(len(value)==0):
         print("line no", line_no , "invalid defnation of labels",sep=' ')
+        d32 = f4()
         error=True
         continue
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     
     if(value[0] not in operations_symbol):
+        d32 = f4()
         print("line no",line_no , '(',value[0],')'," is invalid instruction name ", sep=' ')
         error=True
         continue
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
 
     if(value[0]=="mov" and len(value)>=2):
+        d32 = f4()
         c = value[2][0]
+        d32 = f4()
         if(65<=ord(c)<=90 or 97<=ord(c)<=122):
             value[0]="mov2"
         else:
             value[0]="mov1"
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     
     if (operations[value[0]][1] == "A"):
+        d32 = f4()
         type_A(value)
             
     elif (operations[value[0]][1] == "C"):
         type_C(value)
+        d32 = f4()
         
     elif (operations[value[0]][1] == "B"):
         type_B(value)
 
     elif (operations[value[0]][1] == "D"):
+        d32 = f4()
         type_D(value)
     
     elif (operations[value[0]][1] == "E"):
@@ -326,6 +496,7 @@ for line in code:
         type_F(value)
 
     else:
+        d32 = f4()
         print("line no",line_no,"invalid syntax",sep=' ')
         error=True
 
@@ -337,10 +508,12 @@ for line in code:
 
 labels={}
 variables={}
-
+d32 = f4()
 t=1
 address=-1
-
+a13 = f1()
+a80 = f2()
+a123 = a13 + a80
 if(error==True):
     exit()
 
@@ -349,33 +522,47 @@ if(error==True):
 for line in code:
     if len(line)==0:
         continue
+    d32 = f4()
     value = list(line.split())
     
     if(value[0] in operations_symbol):
         address+=1
 
     if value[0]=="hlt":
+        d32 = f4()
         labels[value[0]+":"]=address
 
     if(value[0][-1]==":"):
         address+=1
         labels[value[0]]=address
+        d32 = f4()
+    d32 = f4()
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
         
 
 #********************************* THIS LOOP WILL STORE THE ADDRESS OF ALL LABELS IN DICTIONARY ***********************
 for line in code:
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     if(len(line)==0):
         continue
     value = list(line.split())
     if value[0]=="var" and len(value)==2:
         variables[value[1]]=t+address
         t+=1
+    d32 = f4()
 
 
 #********************************* THIS IS MAIN LOOP TO COVERT ASSEMBLY INTO BINARY CODE *******************************
 for line in code:
-
+    a13 = f1()
+    a80 = f2()
+    a123 = a13 + a80
     if(len(line)==0):
+        d32 = f4()
         continue
 
     value = list(line.split())
@@ -387,6 +574,7 @@ for line in code:
         if(value[0]=="mov" ):
             if(value[2][0]=="$"):
                 value[0]="mov1"
+                d32 = f4()
             else:
                 value[0]="mov2"
 
@@ -394,33 +582,42 @@ for line in code:
             a = value[1]
             b = value[2][1:]
             b1 = bin(int(b))[2:]
+            d32 = f4()
             s = operations[value[0]][0] + RegAddress[a] + (8-len(b1))*"0" + b1
 
         elif (operations[value[0]][1] == "A"):
             a = value[1]
             b = value[2]
             c = value[3]
+            d32 = f4()
             s = operations[value[0]][0] + "00" + RegAddress[a] + RegAddress[b] + RegAddress[c]
     
         elif (operations[value[0]][1] == "C"):
             a = value[1]
             b = value[2]
+            d32 = f4()
             s = operations[value[0]][0] + "00000" + RegAddress[a] + RegAddress[b]
 
         elif (operations[value[0]][1] == "D"):
             a = value[1]
             b = bin(variables[value[2]])[2:]
+            d32 = f4()
             s = operations[value[0]][0] + RegAddress[a] + (8 - len(b)) * "0" + b
 
         elif (operations[value[0]][1] == "E"):
             a=value[1]
             b=bin(labels[a+":"])[2:]
+            d32 = f4()
             s=operations[value[0]][0] + "000" + (8 - len(b)) * "0" + b
 
         elif (operations[value[0]][1] == "F"):
             s = operations[value[0]][0] + "00000000000"
-
+            d32 = f4()
+        d32 = f4()
         print(s)
+        a13 = f1()
+        a80 = f2()
+        a123 = a13 + a80
 
 
 # ***********************************************THE END********************************************************************
